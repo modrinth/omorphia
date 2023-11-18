55341c12bf31b358a677d5a6da6287ba9e6e4263
tobinio <Tobias.frischmann1@gmail.com>
2023-11-18 14:30:23 +0100

    simplify btn-color class declaration

--- lib/components/base/Button.vue
+++ lib/components/base/Button.vue
@@ -6,15 +6,26 @@ import { computed } from 'vue'
 const props = withDefaults(
   defineProps<{
     link?: string
-    external: boolean
+    external?: boolean
     action?: (event: MouseEvent) => void
-    color: 'default' | 'danger'| 'primary'| 'red'| 'orange'| 'green'| 'blue'| 'purple'| 'gray' | 'secondary' | 'highlight'
-    iconOnly: boolean
-    large: boolean
-    outline: boolean
-    transparent: boolean
-    hoverFilled: boolean
-    hoverFilledOnly: boolean
+    color?:
+      | 'default'
+      | 'danger'
+      | 'primary'
+      | 'red'
+      | 'orange'
+      | 'green'
+      | 'blue'
+      | 'purple'
+      | 'gray'
+      | 'secondary'
+      | 'highlight'
+    iconOnly?: boolean
+    large?: boolean
+    outline?: boolean
+    transparent?: boolean
+    hoverFilled?: boolean
+    hoverFilledOnly?: boolean
   }>(),
   {
     link: undefined,
@@ -42,21 +53,12 @@ const accentedButton = computed(() =>
     :class="{
       'icon-only': iconOnly,
       'btn-large': large,
-      'btn-danger': color === 'danger',
-      'btn-primary': color === 'primary',
-      'btn-secondary': color === 'secondary',
-      'btn-highlight': color === 'highlight',
-      'btn-red': color === 'red',
-      'btn-orange': color === 'orange',
-      'btn-green': color === 'green',
-      'btn-blue': color === 'blue',
-      'btn-purple': color === 'purple',
-      'btn-gray': color === 'gray',
       'btn-transparent': transparent,
       'btn-hover-filled': hoverFilled,
       'btn-hover-filled-only': hoverFilledOnly,
       'btn-outline': outline,
       'color-accent-contrast': accentedButton,
+      [`btn-${color}`]: true,
     }"
     :to="link"
     :target="external ? '_blank' : '_self'"
@@ -71,21 +73,12 @@ const accentedButton = computed(() =>
     :class="{
       'icon-only': iconOnly,
       'btn-large': large,
-      'btn-danger': color === 'danger',
-      'btn-primary': color === 'primary',
-      'btn-secondary': color === 'secondary',
-      'btn-highlight': color === 'highlight',
-      'btn-red': color === 'red',
-      'btn-orange': color === 'orange',
-      'btn-green': color === 'green',
-      'btn-blue': color === 'blue',
-      'btn-purple': color === 'purple',
-      'btn-gray': color === 'gray',
       'btn-transparent': transparent,
       'btn-hover-filled': hoverFilled,
       'btn-hover-filled-only': hoverFilledOnly,
       'btn-outline': outline,
       'color-accent-contrast': accentedButton,
+      [`btn-${color}`]: true,
     }"
     :href="link"
     :target="external ? '_blank' : '_self'"
@@ -100,21 +93,12 @@ const accentedButton = computed(() =>
     :class="{
       'icon-only': iconOnly,
       'btn-large': large,
-      'btn-danger': color === 'danger',
-      'btn-primary': color === 'primary',
-      'btn-secondary': color === 'secondary',
-      'btn-highlight': color === 'highlight',
-      'btn-red': color === 'red',
-      'btn-orange': color === 'orange',
-      'btn-green': color === 'green',
-      'btn-blue': color === 'blue',
-      'btn-purple': color === 'purple',
-      'btn-gray': color === 'gray',
       'btn-transparent': transparent,
       'btn-hover-filled': hoverFilled,
       'btn-hover-filled-only': hoverFilledOnly,
       'btn-outline': outline,
       'color-accent-contrast': accentedButton,
+      [`btn-${color}`]: true,
     }"
     @click="action"
   >
